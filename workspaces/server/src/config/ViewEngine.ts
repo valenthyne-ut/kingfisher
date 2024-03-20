d69653d72fa616ec40b7e3c57f4991189d1fba0b
valenthyne-ut <valenthyne.ut@gmail.com>
2024-03-20 19:18:49 +0000

    Display message if htdocs isn't present

--- workspaces/server/src/config/ViewEngine.ts
+++ workspaces/server/src/config/ViewEngine.ts
@@ -1,4 +1,6 @@
+import { logger } from "@/classes/Logger";
 import express, { Express } from "express";
+import { existsSync } from "fs";
 import { join } from "path";
 import { renderFile } from "pug";
 
@@ -6,21 +8,30 @@ export function setupViewEngine(app: Express) {
 	const cwd = process.cwd();
 
 	const htdocsPath = join(cwd, "/htdocs");
-	const assetsPath = join(htdocsPath, "/assets");
-	const faviconPath = join(htdocsPath, "/favicon.ico");
 
-	app.use("/assets", express.static(assetsPath));
-	app.use("/favicon.ico", express.static(faviconPath));
+	if(existsSync(htdocsPath)) {
+		const assetsPath = join(htdocsPath, "/assets");
+		const faviconPath = join(htdocsPath, "/favicon.ico");
 
-	app.engine("html", renderFile);
-	app.set("view engine", "html");
-	app.set("views", htdocsPath);
+		app.use("/assets", express.static(assetsPath));
+		app.use("/favicon.ico", express.static(faviconPath));
 
-	app
-		.get(["/", "/files", "/settings", "/login"], (request, response) => {
-			return response.render("index.html");
-		})
-		.get("*", (request, response) => { // Fallback 404 route, redirect users to SPA
-			return response.redirect("/");
+		app.engine("html", renderFile);
+		app.set("view engine", "html");
+		app.set("views", htdocsPath);
+
+		app
+			.get(["/", "/files", "/settings", "/login"], (request, response) => {
+				return response.render("index.html");
+			})
+			.get("*", (request, response) => { // Fallback 404 route, redirect users to SPA
+				return response.redirect("/");
+			});
+	} else {
+		logger.warning("Server is starting in API-only mode.");
+
+		app.get("*", (request, response) => {
+			return response.send("<p>This Kingfisher instance is running in API-only mode.</p>");
 		});
+	}
 }
